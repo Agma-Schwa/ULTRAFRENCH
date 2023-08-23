951f79fd324c31909bc634f0bcdfea82be0dff63
Sirraide <aeternalmail@gmail.com>
2023-08-23 09:30:01 +0200

    Add IPA transliterator

--- ULTRAFRENCHER/src/main.cc
+++ ULTRAFRENCHER/src/main.cc
@@ -2,6 +2,7 @@
 #include <clopts.hh>
 #include <ranges>
 #include <string>
+#include <unicode/schriter.h>
 #include <unicode/stringoptions.h>
 #include <unicode/translit.h>
 #include <unicode/unistr.h>
@@ -48,6 +49,21 @@ auto to_utf8(std::u32string_view sv) -> std::string {
     std::string result;
     s.toUTF8String(result);
     return result;
+}
+
+auto to_utf32(const icu::UnicodeString& us) -> std::u32string {
+    std::u32string text;
+    UErrorCode err{U_ZERO_ERROR};
+    text.resize(usz(us.length()));
+    auto sz = us.toUTF32(reinterpret_cast<UChar32*>(text.data()), i32(text.size()), err);
+    if (U_FAILURE(err)) die("Failed to convert text to UTF-32: {}", u_errorName(err));
+    text.resize(usz(sz));
+    return text;
+}
+
+auto utf8(char32_t c) -> std::string {
+    std::u32string_view sv{&c, 1};
+    return to_utf8(sv);
 }
 
 namespace detail {
@@ -146,12 +162,7 @@ void generate() {
     if (U_FAILURE(err)) die("Failed to get NFKD normalizer: {}", u_errorName(err));
 
     /// Convert text to u32.
-    std::u32string text;
-    auto us = icu::UnicodeString::fromUTF8(options::get<"--dict">()->contents);
-    text.resize(usz(us.length()));
-    auto sz = us.toUTF32(reinterpret_cast<UChar32*>(text.data()), i32(text.size()), err);
-    if (U_FAILURE(err)) die("Failed to convert text to UTF-32: {}", u_errorName(err));
-    text.resize(usz(sz));
+    std::u32string text = to_utf32(icu::UnicodeString::fromUTF8(options::get<"--dict">()->contents));
 
     /// Process the text.
     line_buffer buf{text};
@@ -226,10 +237,407 @@ void generate() {
 } // namespace dictionary
 
 namespace ipa {
+static constexpr char32_t Acute = U'́';
+static constexpr char32_t Grave = U'̀';
+static constexpr char32_t Circumflex = U'̂';
+static constexpr char32_t Tilde = U'̃';
+static constexpr char32_t VoicelessBelow = U'̥';
+static constexpr char32_t VoicelessAbove = U'̊';
+static constexpr char32_t Diaeresis = U'̈';
+static constexpr char32_t DotBelow = U'̣';
+
+static constexpr std::u32string_view apos = U"'`’";
+static constexpr std::u32string_view ws = U" \t\v\f\n\r";
+
+class iterator {
+    const char32_t* it;
+    const char32_t* end;
+
+public:
+    iterator(const std::u32string& sv)
+        : it{sv.data()}
+        , end{sv.data() + sv.size()} {}
+
+    bool consume(char32_t c) {
+        if (is(c)) {
+            it++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool consume_any(std::u32string_view sv) {
+        if (is_any(sv)) {
+            it++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool is(std::same_as<char32_t> auto... cs) {
+        auto c = **this;
+        return ((c == cs) or ...);
+    }
+
+    bool is_any(std::u32string_view sv) {
+        return rgs::contains(sv, **this);
+    }
+
+    bool matches(std::u32string_view sv) {
+        return std::u32string_view{it, usz(end - it)}.starts_with(sv);
+    }
+
+    char32_t operator*() const {
+        if (it == end) return 0;
+        return *it;
+    }
+
+    auto operator++() -> char32_t {
+        if (it == end) return 0;
+        return *++it;
+    }
+
+    auto operator++(int) -> char32_t {
+        if (it == end) return 0;
+        return *it++;
+    }
+
+    explicit operator bool() const { return it != end; }
+};
+
+/// Convert a sound to its nasal equivalent (but without
+/// any diacritics)
+constexpr char32_t
+Nasal(char32_t c) {
+    switch (c) {
+        case 'a': return U'ɑ';
+        case 'e': return U'ɛ';
+        case 'i': return U'i';
+        case 'o': return U'ɔ';
+        case 'u': return U'u';
+        case 'y': return U'ʏ';
+        default: return c;
+    }
+}
+
 void translate() {
     std::string_view text = *options::get<"-i">();
+
+    /// Normalise the text before IPA conversion so we have
+    /// to deal with fewer cases. Also convert to lowercase
+    /// and convert ASCII apostrophes to '’'.
+    UErrorCode err{U_ZERO_ERROR};
+    auto normaliser = icu::Transliterator::createInstance(
+        u"NFD; Lower;",
+        UTRANS_FORWARD,
+        err
+    );
+    if (U_FAILURE(err)) die("Failed to get Transliterator: {}", u_errorName(err));
+    auto us = icu::UnicodeString::fromUTF8(text);
+    normaliser->transliterate(us);
+
+    /// Convert it to a u32 string.
+    /// Map the text to IPA.
+    std::u32string ipa, input = to_utf32(us);
+    char32_t c{};
+    [[maybe_unused]] char32_t prev{};
+    iterator it{input};
+
+    /// Helper to handle apostrophe-h combinations. Some letters
+    /// may be followed by '’h', which turns them into fricatives;
+    /// if the apostrophe is not followed by a 'h', then it is simply
+    /// discarded. This handles that case.
+    const auto HandleApostropheH = [&](char32_t base, char32_t fricative) {
+        if (it.consume_any(apos)) {
+            if (it.consume(U'h')) {
+                ipa += fricative;
+                return;
+            }
+        }
+
+        ipa += base;
+    };
+
+    for (; it; prev = c) {
+        switch (c = it++) {
+            /// Skip most punctuation marks.
+            case U'.':
+            case U',':
+            case U':':
+            case U';':
+            case U'-':
+            case U'–':
+            case U'—':
+            case U'/':
+            case U'\\':
+                break;
+
+            /// Skip letters that are not part of the language.
+            case U'g':
+            case U'm':
+            case U'k':
+            case U'p':
+            case U'q':
+            case U'x':
+                break;
+
+            /// Collapse whitespace.
+            case U' ':
+            case U'\t':
+            case U'\v':
+            case U'\f':
+            case U'\n':
+            case U'\r':
+                while (it.is_any(ws)) it++;
+                ipa += U' ';
+                break;
+
+            /// Simple vowels.
+            case U'i':
+            case U'o':
+            case U'u':
+            simple_vowel: {
+                switch (*it) {
+                    case Grave:
+                        it++;
+                        ipa += c;
+                        ipa += VoicelessBelow;
+                        break;
+
+                    case Acute:
+                        it++;
+                        ipa += Nasal(c);
+                        ipa += Tilde;
+                        break;
+
+                    case Circumflex:
+                        it++;
+                        ipa += Nasal(c);
+                        ipa += Tilde;
+                        ipa += Tilde;
+                        break;
+
+                    default:
+                        ipa += c;
+                        break;
+                }
+            } break;
+
+            /// 'a' could be followed by 'u', optionally with
+            /// a diacritic on the 'u' (except a diaeresis), in
+            /// which case it is actually 'o'.
+            case U'a': {
+                if (it.consume(U'u')) {
+                    if (it.consume(Diaeresis)) {
+                        ipa += U'a';
+                        c = U'u';
+                    } else {
+                        c = 'o';
+                    }
+                }
+                goto simple_vowel;
+            }
+
+            /// 'e' is complicated because it has two nasal variants,
+            /// and because it can also be a schwa. Fortunately, NFC
+            /// puts the dot below first, so we can get that out of the
+            /// way early.
+            case U'e': {
+                const bool dot = it.consume(DotBelow);
+                switch (*it) {
+                    /// E-grave is oral 'ɛ'.
+                    case Grave:
+                        it++;
+                        ipa += U'ɛ';
+                        break;
+
+                    case Acute:
+                        it++;
+                        ipa += dot ? U'e' : U'ɛ';
+                        ipa += Tilde;
+                        break;
+
+                    case Circumflex:
+                        it++;
+                        ipa += dot ? U'e' : U'ɛ';
+                        ipa += Tilde;
+                        ipa += Tilde;
+                        break;
+
+                    default:
+                        /// E-dot w/ no nasal diacritic is a schwa.
+                        ipa += dot ? U'ə' : U'e';
+
+                        /// Word-finally, E-dot is voiceless.
+                        if (dot and (not it or it.is_any(ws))) ipa += VoicelessBelow;
+                        break;
+                }
+            } break;
+
+            /// 'y' can be a vowel or a consonant if followed by '’'. Note
+            /// that there may be an acute before the apostrophe.
+            case 'y': {
+                switch (*it) {
+                    case Acute:
+                        it++;
+                        if (it.consume_any(apos)) ipa += U"ɥ̃";
+                        else {
+                            ipa += Nasal(c);
+                            ipa += Tilde;
+                        }
+                        break;
+
+                    default:
+                        if (it.consume_any(apos)) ipa += U"ɥ";
+                        else goto simple_vowel;
+                }
+            } break;
+
+            /// 'b' can be followed by a dot, 'h', or an apostrophe.
+            case U'b': {
+                switch (*it) {
+                    case U'h':
+                        it++;
+                        ipa += U"bʱ";
+                        break;
+
+                    /// If followed by '’h', then this is a fricative. Otherwise,
+                    /// just skip the apostrophe.
+                    case U'’':
+                    case U'\'':
+                    case U'`':
+                        it++;
+                        if (it.consume(U'h')) ipa += U"β";
+                        else ipa += U"b";
+                        break;
+
+                    /// Dot below doesn’t change the pronunciation.
+                    default:
+                        it.consume(DotBelow);
+                        ipa += U'b';
+                        break;
+                }
+            } break;
+
+            case U'c':
+                if (it.consume(Acute)) ipa += U"ɕʶ";
+                else HandleApostropheH(U'ɕ', U'x');
+                break;
+
+            /// 'd' can be followed by a dot, or '’h’.
+            case U'd':
+                if (it.consume(DotBelow)) ipa += 'd';
+                else HandleApostropheH(U'd', U'ð');
+                break;
+
+            case U'f':
+                ipa += U'ɸ';
+                break;
+
+            case U'h':
+                ipa += U'h';
+                break;
+
+            /// We sometimes use dotless ‘j’ so we don’t end up
+            /// with a dot *and* an acute.
+            case U'ȷ':
+            case U'j':
+                ipa += U'ʑ';
+                if (it.consume(Acute)) ipa += U'ʶ';
+                break;
+
+            /// Also handle `ll`.
+            case U'l':
+                if (it.consume(DotBelow)) {
+                    ipa += U"ʎ̝̃";
+                    while (it.matches(U"ḷ")) {
+                        it++;
+                        it++;
+                        ipa += U'ː';
+                    }
+                } else {
+                    ipa += U"ɮ̃";
+                    while (*it == U'l' and not it.matches(U"ḷ")) {
+                        it++;
+                        ipa += U'ː';
+                    }
+                }
+                break;
+
+            case U'ł':
+                ipa += U"ɮ̃ʶ";
+                while (it.consume(U'ł')) ipa += U'ː';
+                break;
+
+            case U'n':
+                ipa += U"n";
+                break;
+
+            case U'r':
+                ipa += U"ɰ";
+                while (it.consume(U'r')) ipa += U'ː';
+                break;
+
+            case U's':
+                if (it.consume(Acute)) {
+                    ipa += U"sʶ";
+                    while (it.matches(U"ś")) {
+                        it++;
+                        it++;
+                        ipa += U'ː';
+                    }
+                } else {
+                    ipa += U's';
+                    while (*it == U's' and not it.matches(U"ś")) {
+                        it++;
+                        ipa += U'ː';
+                    }
+                }
+                break;
+
+            /// 't' is always 't’h'
+            case U't':
+                it.consume_any(apos);
+                it.consume(U'h');
+                ipa += U'θ';
+                break;
+
+            case U'v':
+                if (it.consume(Acute)) ipa += U"βʶ";
+                else {
+                    ipa += U"ʋ̃";
+                    while (*it == U'v' and not it.matches(U"v́")) {
+                        it++;
+                        ipa += U'ː';
+                    }
+                }
+                break;
+
+            case U'w':
+                ipa += U"ɰ̃";
+                break;
+
+            case U'z':
+                ipa += U'z';
+                if (it.consume(Acute)) ipa += U'ʶ';
+                break;
+
+            default:
+                fmt::print(
+                    stderr,
+                    "[ULTRAFRENCHER] Warning: unsupported character U+{:04X}: {}\n",
+                    u32(c),
+                    u32(c)
+                );
+                break;
+        }
+    }
+
+    fmt::print(stdout, "{}\n", to_utf8(ipa));
 }
-}
+} // namespace ipa
 
 int main(int argc, char** argv) {
     options::parse(argc, argv);
