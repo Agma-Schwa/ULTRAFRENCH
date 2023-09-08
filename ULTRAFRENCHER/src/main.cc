e5a709b74cec1cd3944fcf2507961cbad312ddb9
Sirraide <aeternalmail@gmail.com>
2023-09-08 08:41:50 +0200

    [ULTRAFRENCHER] Use FindICU

--- ULTRAFRENCHER/src/main.cc
+++ ULTRAFRENCHER/src/main.cc
@@ -1,19 +1,44 @@
 #include <algorithm>
 #include <clopts.hh>
+#include <fmt/format.h>
 #include <ranges>
 #include <string>
 #include <unicode/schriter.h>
 #include <unicode/stringoptions.h>
 #include <unicode/translit.h>
 #include <unicode/unistr.h>
-#include <utils.hh>
 #include <variant>
 #include <vector>
 
 namespace rgs = std::ranges;
 namespace vws = std::ranges::views;
 
-#define DICT_FILE "DICTIONARY.txt"
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usz = size_t;
+using uptr = uintptr_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isz = ptrdiff_t;
+using iptr = intptr_t;
+
+#define STR_(X) #X
+#define STR(X)  STR_(X)
+
+#define CAT_(X, Y) X##Y
+#define CAT(X, Y)  CAT_(X, Y)
+
+template <typename... arguments>
+[[noreturn]] void die(fmt::format_string<arguments...> fmt, arguments&&... args) {
+    fmt::print(stderr, fmt, std::forward<arguments>(args)...);
+    fmt::print(stderr, "\n");
+    std::exit(1);
+}
 
 void trim(std::u32string& s) {
     while (s.back() == ' ') { s.pop_back(); }
