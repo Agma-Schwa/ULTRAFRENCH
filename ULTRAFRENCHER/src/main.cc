ac7ca7be7bf950f1e22d6c336e0181c5166970d3
Sirraide <aeternalmail@gmail.com>
2025-03-03 21:18:00 +0100

    [ULTRAFRENCHER] Move UF-specific macros out of the dictionary library

--- ULTRAFRENCHER/src/main.cc
+++ ULTRAFRENCHER/src/main.cc
@@ -12,10 +12,19 @@
 using namespace base;
 
 struct UFOps : dict::LanguageOps {
+    auto handle_unknown_macro(dict::TeXToHtmlConverter& conv, std::string_view macro) -> Result<> override;
     auto to_ipa(std::string_view word) -> std::string override {
         return ipa::Translate(word, false);
     }
 };
+
+auto UFOps::handle_unknown_macro(dict::TeXToHtmlConverter& conv, std::string_view macro) -> Result<> {
+    if (macro == "pf") Try(conv.single_argument_macro_to_tag("f-pf"));
+    else if (macro == "L") conv.drop_empty_and_append_raw("<f-mut><sup>L</sup></f-mut>");
+    else if (macro == "N") conv.drop_empty_and_append_raw("<f-mut><sup>N</sup></f-mut>");
+    else return LanguageOps::handle_unknown_macro(conv, macro);
+    return {};
+}
 
 int main(int argc, char** argv) {
     using namespace command_line_options;
