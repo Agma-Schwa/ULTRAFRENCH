7fef740c872d1314f93d19927ce6fb8ff46c25df
Sirraide <aeternalmail@gmail.com>
2025-03-03 21:41:20 +0100

    [ULTRAFRENCHER] Move \pf insertion out of the library

--- ULTRAFRENCHER/src/main.cc
+++ ULTRAFRENCHER/src/main.cc
@@ -13,6 +13,7 @@ using namespace base;
 
 struct UFOps : dict::LanguageOps {
     auto handle_unknown_macro(dict::TeXToHtmlConverter& conv, std::string_view macro) -> Result<> override;
+    auto preprocess_full_entry(std::vector<std::u32string>&) -> Result<> override;
     auto to_ipa(std::string_view word) -> std::string override {
         return ipa::Translate(word, false);
     }
@@ -23,6 +24,20 @@ auto UFOps::handle_unknown_macro(dict::TeXToHtmlConverter& conv, std::string_vie
     else if (macro == "L") conv.drop_empty_and_append_raw("<f-mut><sup>L</sup></f-mut>");
     else if (macro == "N") conv.drop_empty_and_append_raw("<f-mut><sup>N</sup></f-mut>");
     else return LanguageOps::handle_unknown_macro(conv, macro);
+    return {};
+}
+
+auto UFOps::preprocess_full_entry(std::vector<std::u32string>& parts) -> Result<> {
+    // If this is a single word, and the field contains no backslashes,
+    // wrap it with '\pf{}'. That takes care of this field for most words
+    // (conversely, more complex etymologies often don’t start w/ a PF word).
+    // Etymology is empty; don’t do anything here.
+    if (auto& part = parts[+dict::FullEntry::Part::EtymPart]; not part.empty()) {
+        // If the etymology contains no spaces or macros, it is likely just
+        // a single French word, so insert \pf.
+        if (not part.contains(U' ') and not part.contains(U'\\'))
+            part = U"\\pf{" + std::move(part) + U"}"; // No Utf32 std::format :(
+    }
     return {};
 }
 
