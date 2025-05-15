c0b7ade1512e4dd3fd9887401005f18da6186df5
Sirraide <aeternalmail@gmail.com>
2025-05-15 07:45:21 +0200

    [DICTIONARY] Rename dictionary file

--- SCRIPTS/GENERATE-DICTIONARY.sh
+++ SCRIPTS/GENERATE-DICTIONARY.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.txt --json          > DICTIONARY.json
-./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.txt --json --minify > DICTIONARY.min.json
-./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.txt                 > DICTIONARY.tex
+./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.dict.txt --json          > DICTIONARY.json
+./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.dict.txt --json --minify > DICTIONARY.min.json
+./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.dict.txt                 > DICTIONARY.tex
