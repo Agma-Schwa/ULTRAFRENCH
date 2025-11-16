bdaa02aef48bf13c4c8bc69cf5d7b907d88ad568
Sirraide <aeternalmail@gmail.com>
2025-11-16 01:04:10 +0100

    Update generation script

--- SCRIPTS/GENERATE-DICTIONARY.sh
+++ SCRIPTS/GENERATE-DICTIONARY.sh
@@ -3,15 +3,15 @@
 set -eu
 
 GENERATE() {
-    ./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY.dict.txt "$@"
+    ./ULTRAFRENCHER/ULTRAFRENCHER --dict DICTIONARY/DICTIONARY.dict.txt "$@"
 }
 
 ## Generate JSON Dictionary.
-GENERATE --json          > DICTIONARY.json
-GENERATE --json --minify > DICTIONARY.min.json
+GENERATE --json          > DICTIONARY/DICTIONARY.json
+GENERATE --json --minify > DICTIONARY/DICTIONARY.min.json
 
 ## Generate Typst Dictionary.
 DICTIONARY_CONTENTS='#import "DICTIONARY-DEFS.typ" : *'
 DICTIONARY_CONTENTS+=$'\n'
 DICTIONARY_CONTENTS+="$(GENERATE)"
-echo "$DICTIONARY_CONTENTS" > DICTIONARY.typ
+echo "$DICTIONARY_CONTENTS" > DICTIONARY/DICTIONARY.typ
